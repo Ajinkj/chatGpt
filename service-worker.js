7a6901b62379fad7e716e310b15751effe0a6b0d
Ajin k j <61120485+Ajinkj@users.noreply.github.com>
2023-07-09 19:05:13 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+// service-worker.js
+
+var CACHE_NAME = 'chatgpt-pwa-v1';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/chat.png'
+  // Add more files and assets that should be cached for offline use
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('chatgpt-pwa-') && cacheName !== CACHE_NAME;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
